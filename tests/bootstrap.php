6f26dd1ba692f74b52db0785f8e6df4b272934eb
Mattia Basone <mattia.basone@gmail.com>
2022-04-29 18:25:00 +0200

    restore

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
-
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?'.PHP_EOL;
     exit(1);
